1733d34cf1cea12deeed9fa009536873ce986855
nukkeldev <110308379+nukkeldev@users.noreply.github.com>
2025-02-17 01:26:14 -0800

    pip-shell

---
+++ shells/pip-shell.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import <nixpkgs> {} }:
+(pkgs.buildFHSUserEnv {
+  name = "pipzone";
+  targetPkgs = pkgs: (with pkgs; [
+    python312
+    python312Packages.pip
+    python312Packages.virtualenv
+  ]);
+  runScript = "bash";
+}).env
+
