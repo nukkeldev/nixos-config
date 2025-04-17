1a0d303508eef0c1cf71a86f362710aed87b95f3
nukkeldev <110308379+nukkeldev@users.noreply.github.com>
2025-04-16 21:49:29 -0700

    Updated and ported hyprland config to nix

--- home/modules/default.nix
+++ home/modules/default.nix
@@ -1,5 +1,6 @@
 {...}: {
   imports = [
+    ./hyprland
     ./waybar
     ./starship
   ];
---
+++ home/modules/hyprland/default.nix
@@ -0,0 +1,154 @@
+{...}: {
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      monitor = [
+        ",highres,auto,1.25"
+      ];
+
+      xwayland = {
+        force_zero_scaling = true;
+      };
+
+      exec-once = [
+        "waybar"
+        "hypridle"
+        "hyprpaper"
+      ];
+
+      env = [
+        "XCURSOR_SIZE,24"
+        "HYPRCURSOR_SIZE,24"
+      ];
+
+      general = {
+        gaps_in = 5;
+        gaps_out = 10;
+        border_size = 2;
+        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.inactive_border" = "rgba(595959aa)";
+        resize_on_border = false;
+        allow_tearing = false;
+        layout = "dwindle";
+      };
+
+      decoration = {
+        rounding = 0;
+      };
+
+      dwindle = {
+        pseudotile = true;
+        preserve_split = true;
+      };
+
+      misc = {
+        force_default_wallpaper = 0;
+        disable_hyprland_logo = false;
+      };
+
+      input = {
+        kb_layout = "us";
+        kb_variant = "";
+        kb_model = "";
+        kb_options = "";
+        kb_rules = "";
+        repeat_delay = 300;
+        follow_mouse = 1;
+
+        touchpad = {
+          natural_scroll = true;
+          scroll_factor = 0.5;
+          clickfinger_behavior = true;
+        };
+      };
+
+      gestures = {
+        workspace_swipe = true;
+      };
+
+      "$mainMod" = "SUPER";
+
+      bind = [
+        "$mainMod, E, exec, ghostty"
+        "$mainMod SHIFT, Q, killactive,"
+        "$mainMod SHIFT, M, exit,"
+        "$mainMod, V, togglefloating,"
+        "$mainMod, P, pseudo,"
+        "$mainMod, J, togglesplit,"
+        "$mainMod, W, exec, firefox"
+        "$mainMod SHIFT, J, exec, idea-ultimate"
+        "$mainMod SHIFT, K, exec, keepassxc"
+        "$mainMod, L, exec, hyprlock"
+        "$mainMod SHIFT, C, exec, hyprpicker"
+        "$mainMod SHIFT, B, exec, \"killall hyprpaper ; hyprpaper\""
+
+        "$mainMod, left, movefocus, l"
+        "$mainMod, right, movefocus, r"
+        "$mainMod, up, movefocus, u"
+        "$mainMod, down, movefocus, d"
+
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
+
+        "$mainMod SHIFT, 1, movetoworkspace, 1"
+        "$mainMod SHIFT, 2, movetoworkspace, 2"
+        "$mainMod SHIFT, 3, movetoworkspace, 3"
+        "$mainMod SHIFT, 4, movetoworkspace, 4"
+        "$mainMod SHIFT, 5, movetoworkspace, 5"
+        "$mainMod SHIFT, 6, movetoworkspace, 6"
+        "$mainMod SHIFT, 7, movetoworkspace, 7"
+        "$mainMod SHIFT, 8, movetoworkspace, 8"
+        "$mainMod SHIFT, 9, movetoworkspace, 9"
+        "$mainMod SHIFT, 0, movetoworkspace, 10"
+
+        "$mainMod, S, togglespecialworkspace, magic"
+        "$mainMod SHIFT, S, movetoworkspace, special:magic"
+
+        "$mainMod, mouse_down, workspace, e+1"
+        "$mainMod, right, workspace, e+1"
+        "$mainMod, mouse_up, workspace, e-1"
+        "$mainMod, left, workspace, e-1"
+      ];
+
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
+
+      bindel = [
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ",XF86MonBrightnessUp, exec, brightnessctl s 10%+"
+        ",XF86MonBrightnessDown, exec, brightnessctl s 10%-"
+      ];
+
+      bindl = [
+        ", XF86AudioNext, exec, playerctl next"
+        ", XF86AudioPause, exec, playerctl play-pause"
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioPrev, exec, playerctl previous"
+      ];
+
+      windowrule = [
+        "float, title:^KeePassXC$"
+        "center 1, title:^KeePassXC$"        
+        "size 40% 30%, title:^KeePassXC$"
+        
+        "float, title:^nm-connection-editor$"
+        "float, title:^gwenview$"
+        "suppressevent maximize, class:.*"
+        "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+      ];
+    };
+  };
+}
